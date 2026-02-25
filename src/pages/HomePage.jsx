8f5243fda00417924e7149e1e8f7e93bc9f3ca41
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-25 12:19:50 +0530

    created homepage for demo

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Welcome to FashionStore</h1>
+      <p className="text-gray-600">Home page coming soon...</p>
+    </div>
+  );
+};
+
+export default HomePage;
