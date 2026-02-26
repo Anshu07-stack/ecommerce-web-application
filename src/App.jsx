8b71c773ae43deac70436cd6587eea5fced5b56c
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-26 21:20:57 +0530

    feat: integrate CartDrawer globally in App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import AppRoutes from './routes/AppRoutes';
+import CartDrawer from './components/features/cart/CartDrawer';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <AppRoutes />
+        <CartDrawer /> 
       </BrowserRouter>
     </Provider>
   );
